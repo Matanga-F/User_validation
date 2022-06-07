1f6fa372d5e2903365a50a5703bdd7fdc847c11a
Matanga-F <matangafulufhelo@gmail.com>
2022-06-07 11:32:28 +0200

    added src folder

---
+++ src/main.js
@@ -0,0 +1,31 @@
+let elUsername = document.getElementById("username")
+function checkLength(minLength){
+    let elMsg = document.getElementById("invalid");
+    if (elUsername.value.length < minLength && elUsername.value != ""){
+        elMsg.textContent = "Username must be " + minLength + " characters long or more."
+    }else{
+        elMsg.textContent = ""; 
+    }
+}
+
+// Password Checker.
+let elPassword = document.getElementById("new_password");
+let elConfirmPassword = document.getElementById("confirm_password");
+
+function passwordConfirm() {
+    let elMsg = document.getElementById("invalid_password");
+    if (elPassword.value != elConfirmPassword.value && elConfirmPassword.value != "") {
+        elMsg.textContent = "Password don't match!!";
+    }else {
+        elMsg.textContent = ""; 
+    }
+}
+
+
+
+
+elUsername.addEventListener("blur", () =>{ 
+    checkLength(5)
+}, false);
+
+elConfirmPassword.addEventListener("blur", passwordConfirm, false);
